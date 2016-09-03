ea34ef3260407b090f2a559fdd695ae6d8baa071
Orit Blum <oritsa@gmail.com>
2016-09-03 10:52:54 +0300

    deleting unneeded imports

--- src/com/couponprojectserver/facadeServers/CompanyFacadeServer.java
+++ src/com/couponprojectserver/facadeServers/CompanyFacadeServer.java
@@ -3,14 +3,11 @@ package com.couponprojectserver.facadeServers;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-
-import org.eclipse.persistence.platform.database.oracle.annotations.NamedPLSQLStoredProcedureQueries;
 
 import java.time.LocalDate;
 import java.util.Collection;
