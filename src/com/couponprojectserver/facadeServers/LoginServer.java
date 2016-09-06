42a12b18600151d922f86dd231b2e26167ccbef4
Orit Blum <oritsa@gmail.com>
2016-09-06 20:51:51 +0300

    Login service

--- src/com/couponprojectserver/facadeServers/LoginServer.java
+++ src/com/couponprojectserver/facadeServers/LoginServer.java
@@ -5,6 +5,7 @@ import javax.websocket.server.PathParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 
+import com.couponproject.constants.ClientType;
 import com.couponproject.facade.AdminFacade;
 import com.couponproject.facade.CompanyFacade;
 import com.couponproject.facade.CustomerFacade;
@@ -19,24 +20,38 @@ public class LoginServer {
 	@Context 
 	private HttpServletRequest request;
 	
-	public void login(@PathParam("name") String name, @PathParam("password") String password, 
-			@PathParam("clientType") String clientType) throws WrongClientTypeException{
-		//TODO: enum??
-		if(clientType.equals("admin")){
-			AdminFacade adminFacade = CouponSystem.getInstance().loginAsAdmin(name, password);
-			request.getSession().setAttribute(Facade_Attr, adminFacade);
-		}else{
-			if(clientType.equals("company")){
-				CompanyFacade compFacade = CouponSystem.getInstance().loginAsCompany(name, password);
-				request.getSession().setAttribute(Facade_Attr, compFacade);
-			}else{
-				if(clientType.equals("customer")){
-					CustomerFacade custFacade = CouponSystem.getInstance().loginAsCustomer(name, password);
-					request.getSession().setAttribute(Facade_Attr, custFacade);
-				}
-				else throw new WrongClientTypeException("The Client Type should be admin, company or custoemr");
-			}
+	public void login (@PathParam("name") String name, @PathParam("password") String password, 
+			@PathParam("clientType") ClientType clientType){
+		switch(clientType){
+			case Admin: 
+				loginAsAdmin(name, password);
+				break;
+			case Company:
+				loginAsCompany(name, password);
+				break;	
+			case Customer:
+				loginAsCustomer(name, password);
+				break;
 		}
+	}
+
+	private void loginAsAdmin(String name, String password) {
+		AdminFacade adminFacade = CouponSystem.getInstance().loginAsAdmin(name, password);
+		request.getSession().setAttribute(Facade_Attr, adminFacade);
+		//TODO: cookies
+		//TODO: 
+	}
+
+	private void loginAsCompany(String name, String password) {
+		CompanyFacade compFacade = CouponSystem.getInstance().loginAsCompany(name, password);
+		request.getSession().setAttribute(Facade_Attr, compFacade);
+		//TODO: cookies
+	}
+	
+	private void loginAsCustomer(String name, String password) {
+		CustomerFacade custFacade = CouponSystem.getInstance().loginAsCustomer(name, password);
+		request.getSession().setAttribute(Facade_Attr, custFacade);
+		//TODO: cookies
 	}
 
 }
