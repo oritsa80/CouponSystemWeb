858743188dee16bc5637a6895794bbc015a10c43
Avi <Avi@10.30.0.157>
2016-09-07 17:49:21 +0300

    login server

--- src/com/couponprojectserver/facadeServers/LoginServer.java
+++ src/com/couponprojectserver/facadeServers/LoginServer.java
@@ -2,56 +2,68 @@ package com.couponprojectserver.facadeServers;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.websocket.server.PathParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 
 import com.couponproject.constants.ClientType;
 import com.couponproject.facade.AdminFacade;
 import com.couponproject.facade.CompanyFacade;
 import com.couponproject.facade.CustomerFacade;
 import com.couponproject.system.CouponSystem;
-import com.couponprojectserver.exceptions.WrongClientTypeException;
 
 @Path("/login")
 public class LoginServer {
 	
+	@Context private HttpServletRequest request;
+	
 	private static final String Facade_Attr = "FACADE";
-	
-	@Context 
-	private HttpServletRequest request;
-	
-	public void login (@PathParam("name") String name, @PathParam("password") String password, 
-			@PathParam("clientType") ClientType clientType){
+
+	@GET
+	@Path("/{name}/{password}/{clientType}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String login (@PathParam("name") String name, 
+						@PathParam("password") String password, 
+						@PathParam("clientType") ClientType clientType){
+		
 		switch(clientType){
-			case Admin: 
-				loginAsAdmin(name, password);
-				break;
-			case Company:
-				loginAsCompany(name, password);
-				break;	
-			case Customer:
-				loginAsCustomer(name, password);
-				break;
+			case Admin: 	return loginAsAdmin(name, password);
+			case Company:	return loginAsCompany(name, password);
+			case Customer:	return loginAsCustomer(name, password);
+		}
+		return "Failure"; 
+	}
+	
+	private String loginAsAdmin(String name, String password) {
+		AdminFacade adminFacade = CouponSystem.getInstance().loginAsAdmin(name, password);
+		if (adminFacade == null) {
+			return "Failure"; 
+		} else {
+			request.getSession().setAttribute(Facade_Attr, adminFacade);
+			return "success";
 		}
 	}
 
-	private void loginAsAdmin(String name, String password) {
-		AdminFacade adminFacade = CouponSystem.getInstance().loginAsAdmin(name, password);
-		request.getSession().setAttribute(Facade_Attr, adminFacade);
-		//TODO: cookies
-		//TODO: 
-	}
-
-	private void loginAsCompany(String name, String password) {
+	private String loginAsCompany(String name, String password) {
 		CompanyFacade compFacade = CouponSystem.getInstance().loginAsCompany(name, password);
-		request.getSession().setAttribute(Facade_Attr, compFacade);
-		//TODO: cookies
-	}
-	
-	private void loginAsCustomer(String name, String password) {
-		CustomerFacade custFacade = CouponSystem.getInstance().loginAsCustomer(name, password);
-		request.getSession().setAttribute(Facade_Attr, custFacade);
-		//TODO: cookies
+		if (compFacade == null) {
+			return "Failure";
+		} else {
+			request.getSession().setAttribute(Facade_Attr, compFacade);
+			return "success";
+		}
+
 	}
 
+	private String loginAsCustomer(String name, String password) {
+		CustomerFacade custFacade = CouponSystem.getInstance().loginAsCustomer(name, password);
+		if (custFacade == null) {
+			return "Failure";
+		} else {
+			request.getSession().setAttribute(Facade_Attr, custFacade);
+			return "success";
+		}
+	}
 }
